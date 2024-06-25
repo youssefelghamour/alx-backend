5f77f63f602d3e782173a9b4258fe8a76e8ee2f9
youssefelghamour <youssefelghamour@gmail.com>
2024-06-25 14:50:18 +0000

    add 1-fifo_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,7 +16,7 @@ class BasicCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """  """
+        """ returns the value in the dictionary that is linked to key """
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" FIFOCache module """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ basic caching system """
+
+    def __init__(self):
+        """ initialisation """
+        super().__init__()
+
+    def put(self, key, item):
+        """ assigns to the dictionary the item value for the key key """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            # convert the dict to a list of the keys
+            # and get get the key of the first item
+            first_key = list(self.cache_data.keys())[0]
+
+            # delete and return the first item
+            first_item = self.cache_data.pop(first_key)
+
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """ returns the value in the dictionary that is linked to key """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
