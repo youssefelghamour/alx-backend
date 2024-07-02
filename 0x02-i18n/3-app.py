f6f5b9851b5a0b879502a823c3af88264f765230
youssefelghamour <youssefelghamour@gmail.com>
2024-07-02 21:28:55 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Basic flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ flask babel config """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+# takes the Config class and loads all attributes defined
+# within it into the Flask application's configuration
+app.config.from_object(Config)
+# Babel inherits the above configs from the flask app
+babel = Babel(app)
+
+
+# this decorator is equivalent to doing:
+# babel = Babel(app, locale_selector=get_locale)
+@babel.localeselector
+def get_locale():
+    """ determines the best match with our supported languages """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ render index page """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
