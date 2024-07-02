cca1cddaf3684d4e3eb41a76e4365632813acf84
youssefelghamour <youssefelghamour@gmail.com>
2024-07-02 17:49:24 +0000

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Basic flask app """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ render index page """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
