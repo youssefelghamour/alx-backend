d9d8c22f2c527681f9d1a16e7c9038e8b11b1e57
youssefelghamour <youssefelghamour@gmail.com>
2024-07-30 14:06:38 -0700

    4. Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  
+  client.hset('HolbertonSchools', 'Portland', 50, print);
+  client.hset('HolbertonSchools', 'Seattle', 80, print);
+  client.hset('HolbertonSchools', 'New York', 20, print);
+  client.hset('HolbertonSchools', 'Bogota', 20, print);
+  client.hset('HolbertonSchools', 'Cali', 40, print);
+  client.hset('HolbertonSchools', 'Paris', 2, print);
+
+  client.hgetall('HolbertonSchools', (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res);
+    }
+  });
+});
