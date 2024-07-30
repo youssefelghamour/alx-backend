53e5c1d741d6d5c75eb2fafd33bf2ee264f23217
youssefelghamour <youssefelghamour@gmail.com>
2024-07-30 11:36:15 -0700

    2. Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, value) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(value);
+    }
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
